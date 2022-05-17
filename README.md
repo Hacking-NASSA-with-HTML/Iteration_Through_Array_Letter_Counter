e4345491a9765880d3fe5fa412e2ff3a4a0da740
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-05-18 09:49:49 +1100

    feat: add double iteration through array

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Iteration_Through_Array_Letter_Counter
+
+Run it as usual javascript code :)
---
+++ index.js
@@ -0,0 +1,14 @@
+let arr = ["python", "c++", "c", "scala", "java"]	
+let find_the_letter = (arr, letter) => {
+	counter = 0;
+	for (let x = 0; x < arr.length; x++) {
+		for (let y = 0; y < arr[x].length; y++) {
+			if (letter == arr[x][y]) {
+				counter = counter + 1;
+			}
+		}
+	}
+	console.log(`В этом списке ${counter} ${letter}`);
+}
+
+find_the_letter(arr, "a");
